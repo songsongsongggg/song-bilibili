75479c15851cd7f908c89ddf65fde3573706e5c1
song <songsongsongggg@163.com>
2022-07-18 15:16:17 +0800

    1-3通用功能：json信息转换配置、全局异常处理配置

---
+++ song-bilibili/song-bilibili-service/src/main/java/com/song/bilibili/service/config/JsonHttpMessageConverterConfig.java
@@ -0,0 +1,50 @@
+package com.song.bilibili.service.config;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.alibaba.fastjson.serializer.SerializerFeature.DisableCircularReferenceDetect;
+
+@Configuration
+public class JsonHttpMessageConverterConfig {
+
+    public static void main(String[] args){
+        List<Object> list = new ArrayList<>();
+        Object o = new Object();
+        list.add(o);
+        list.add(o);
+        System.out.println(list.size());
+        System.out.println(JSONObject.toJSONString(list));
+        System.out.println(JSONObject.toJSONString(list,SerializerFeature.DisableCircularReferenceDetect));
+
+
+    }
+
+    @Bean
+    @Primary
+    public HttpMessageConverters fastJsonHttpMessageConverters(){
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.MapSortField,
+                DisableCircularReferenceDetect
+        );
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return new HttpMessageConverters(fastConverter);
+    }
+
+}
